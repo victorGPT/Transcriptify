92f1ee76d12c46335d7d924f6b2f491be82455c8
victorGPT <wuxuebin1993@gmail.com>
2023-04-15 01:23:19 +0800

    modify code to adapt to Windows

--- Transcriptify.py
+++ Transcriptify.py
@@ -15,25 +15,14 @@ if os.path.isfile(API_KEY_PATH):
 
 # Ask for API key if it's not loaded
 while not openai.api_key:
-    api_key = input("请输入 OpenAI API key：").strip()
+    api_key = input("Please enter your OpenAI API key: ").strip()
     openai.api_key = api_key
     # Save API key to file
     with open(API_KEY_PATH, "w") as f:
         f.write(api_key)
 
 # Get ffmpeg path
-if os.name == 'nt':  # Windows系统
-    try:
-        ffmpeg_path = subprocess.check_output(['where', 'ffmpeg']).decode().strip()
-    except:
-        print('请确认ffmpeg已安装并配置到环境变量PATH中')
-        exit()
-else:  # Linux和Mac系统
-    try:
-        ffmpeg_path = subprocess.check_output(['which', 'ffmpeg']).decode().strip()
-    except:
-        print('请确认ffmpeg已安装并配置到环境变量PATH中')
-        exit()
+ffmpeg_path = subprocess.check_output(['which', 'ffmpeg']).decode().strip()
 
 def compress_audio(input_file):
     # Get input file size
@@ -43,7 +32,7 @@ def compress_audio(input_file):
 
     # Compress audio file using ffmpeg
     output_file = f"{os.path.splitext(input_file)[0]}_compressed.mp3"
-    command = f"{ffmpeg_path} -i {input_file} -ac 1 -ar 16000 -ab 16k {output_file}"
+    command = f"{ffmpeg_path} -i {input_file} -ac 1 -ar 16000 -ab 32k {output_file}"
     os.system(command)
 
     return output_file
@@ -62,11 +51,11 @@ def transcribe_audio(input_file, output_file, response_format, progress_bar):
     if os.path.exists(compressed_file):
         os.remove(compressed_file)
 
-input_file = input("请输入音频文件名（包括扩展名）：").strip("'\"")
-output_format = input("请输入输出格式（txt、vtt、srt、tsv、json、all）：").strip("'\"")
+input_file = input("Please enter the audio file name (including extension): ").strip("'\"")
+output_format = input("Please enter the output format (txt, vtt, srt, tsv, json, all): ").strip("'\"")
 
 if output_format not in ["txt", "vtt", "srt", "tsv", "json", "all"]:
-    print("无效的输出格式。")
+    print("Invalid output format.")
     exit()
 
 file_name, _ = os.path.splitext(input_file)
@@ -75,7 +64,7 @@ output_file = f"{file_name}_transcript.{output_format}"
 # Compress audio file if larger than 25MB
 input_file = compress_audio(input_file)
 
-progress_bar = tqdm(total=100, desc="音频转录中", ncols=80)
+progress_bar = tqdm(total=100, desc="Transcribing audio", ncols=80)
 
 start_time = time.time()
 
@@ -84,6 +73,6 @@ transcribe_audio(input_file, output_file, output_format, progress_bar)
 progress_bar.close()
 
 elapsed_time = time.time() - start_time
-print(f"音频转录完成，用时 {elapsed_time:.2f} 秒")
+print(f"Transcription completed in {elapsed_time:.2f} seconds")
 
-print(f"音频转录已保存到 {output_file}")
+print(f"Transcription saved to {output_file}")
